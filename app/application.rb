ecb78c5c6b44c211ec6594882069f747c43f8375
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-03-11 04:33:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
    resp = Rack::Response.new
     current_time = Time.now.to_i
-  midnight = Time.now.beginning_of_day.to_i
+  morning = Time.now.beginning_of_day.to_i
   noon = Time.now.middle_of_day.to_i
    if time.parse < 1200
      resp.write "Good Morning"
