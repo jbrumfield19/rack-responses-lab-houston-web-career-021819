d46f59965d84b75e5cf0abbfc5a774c030a33629
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-03-11 04:37:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,8 @@
 class Application
   def call(env)
    resp = Rack::Response.new
-    current_time = Time.now.to_i
-  midnight = Time.now.beginning_of_day.to_i
-  noon = Time.now.middle_of_day.to_i
-   if midnight.upto(noon).include?(current_time)
+   time
+   if 
      resp.write "Good Morning"
    else
      resp.write "Good Afternoon"
