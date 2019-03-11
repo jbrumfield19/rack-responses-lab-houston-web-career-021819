d85bf7e411e68ac47c300891abb8868353d6cd71
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-03-11 04:22:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
    resp = Rack::Response.new
-   if Time.now < 1200
+   if Time.now < 12
      resp.write "Good Morning"
    else
      resp.write "Good Afternoon"
